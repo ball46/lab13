a629f1d62ade789940c83c4fca59b80eaa0a735b
charnchol <khumball46@gmail.com>
2021-12-22 13:31:07 +0700

    lab 13-2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a, int b){
+    if(a%b == 0) return b;
+    else if(b%a == 0) return a;
+    else{
+        int c = 0;
+        if(a>b) c = a%b;
+        else c = b%a;
+        a = b;
+        b = c;
+        return gcd(a,b);
+    }
 }
